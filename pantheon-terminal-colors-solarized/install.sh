be7ed96800bdc3a0a60aa7bd5891f1947f9d0807
Simon Hitzemann <github@simon.hitzemann.org>
2026-07-08 12:43:57 +0200

    Reset repository for chezmoi

--- pantheon-terminal-colors-solarized/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-gsettings set org.pantheon.terminal.settings palette "#070736364242:#DCDC32322F2F:#858599990000:#B5B589890000:#26268B8BD2D2:#D3D336368282:#2A2AA1A19898:#EEEEE8E8D5D5:#00002B2B3636:#CBCB4B4B1616:#58586E6E7575:#65657B7B8383:#838394949696:#6C6C7171C4C4:#9393A1A1A1A1:#FDFDF6F6E3E3"
-gsettings set org.pantheon.terminal.settings foreground "#838394949696"
-gsettings set org.pantheon.terminal.settings background "#00002B2B3636"
-gsettings set org.pantheon.terminal.settings cursor-color "#838394949696"
